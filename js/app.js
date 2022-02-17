18d48ec521ece4ef6ff7695b126edbbb91c95b11
Fatema-Nawrin <nawrin4000@gmail.com>
2022-02-17 19:42:13 +0600

    savings calculation added

--- js/app.js
+++ js/app.js
@@ -1,23 +1,55 @@
-// const incomeInput = document.getElementById('income-input');
+
 let totalExpenses = document.getElementById('total-expenses');
 let balance = document.getElementById('balance');
 let remainingBalance = document.getElementById('remaining-balance')
 
+// function for calculating balance after expense and savings 
 function remainingValue(firstValue, secondValue) {
     const result = (firstValue - secondValue);
     return result.toFixed(2);
 }
+
+// function for all inputfield
+// 
 function getInputValue(item) {
-    const itemInput = parseFloat(document.getElementById(item + '-input').value);
-    return itemInput;
+    const inputField = document.getElementById(item + '-input');
+    const inputValue = inputField.value;
+
+    // Error message for not putting number
+    if (isNaN(inputValue)) {
+        alert('Enter only number');
+        inputField.value = '';
+    }
+    // Error message for negative number
+    else if (inputValue < 0) {
+        alert('Please, Enter a positive number in ' + item + ' input field');
+        inputField.value = '';
+    }
+    else {
+        return parseFloat(inputValue);
+    }
 }
 
 document.getElementById('calculate-button').addEventListener('click', function () {
-    const totalInputExpenses = getInputValue('food') + getInputValue('rent') + getInputValue('clothes')
-    //  parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothesInput.value);
+    const totalIncome = getInputValue('income');
+    const totalInputExpenses = getInputValue('food') + getInputValue('rent') + getInputValue('clothes');
+    const totalBalance = remainingValue(totalIncome, totalInputExpenses);
+    // showing 0 instead of NaN
+    if (isNaN(totalInputExpenses) || isNaN(totalBalance)) {
+        totalInputExpenses = 0;
+        totalBalance = 0;
+    }
+
     totalExpenses.innerText = totalInputExpenses;
-    const totalIncome = getInputValue('income')
-    balance.innerText = remainingValue(totalIncome, totalInputExpenses);
+    // Error message if balance is larger than income
+    if (totalBalance < 0) {
+        alert('Expenses can not be greater than income');
+        balance.innerText = 0;
+        totalExpenses.innerText = 0;
+    }
+    else {
+        balance.innerText = totalBalance;
+    }
 })
 
 document.getElementById('save-button').addEventListener('click', function () {
@@ -26,4 +58,7 @@ document.getElementById('save-button').addEventListener('click', function () {
     const totalSaving = parseFloat(savingAmount.innerText);
     const totalBalance = parseFloat(balance.innerText);
     remainingBalance.innerText = remainingValue(totalBalance, totalSaving)
+
+
 })
+
